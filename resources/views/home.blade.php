59b88cb21a50067e808e7a7018223dd79c599955
aldrvk <aldriknoelsianipar@gmail.com>
2025-06-01 21:03:22 +0700

    membuat model halaman dan card itinerary

---
+++ resources/views/home.blade.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>Divergent Traveller</title>
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com" rel="preconnect">
+  <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Cardo:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+</head>
+
+<body class="index-page">
+
+  <header id="header" class="header d-flex align-items-center sticky-top">
+    <div class="container-fluid position-relative d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center me-auto me-xl-0 px-3">
+        <h1 class="sitename">Divergent Traveller</h1>
+      </a>
+
+      <nav id="navmenu" class="navmenu">
+        <ul>
+          <li class="px-3"><a href="index.html" class="active">Travel Itineraries<br></a></li>
+          <li class="px-3"><a href="about.html">About</a></li>
+          <li class="dropdown px-3"><a href="gallery.html"><span>Gallery</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+            <ul>
+              <li><a href="gallery.html">Nature</a></li>
+              <li><a href="gallery.html">People</a></li>
+              <li><a href="gallery.html">Architecture</a></li>
+              <li><a href="gallery.html">Animals</a></li>
+              <li><a href="gallery.html">Sports</a></li>
+              <li><a href="gallery.html">Travel</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                  <li><a href="#">Deep Dropdown 1</a></li>
+                  <li><a href="#">Deep Dropdown 2</a></li>
+                  <li><a href="#">Deep Dropdown 3</a></li>
+                  <li><a href="#">Deep Dropdown 4</a></li>
+                  <li><a href="#">Deep Dropdown 5</a></li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li class="px-3"><a href="services.html">Services</a></li>
+          <li class="px-3"><a href="contact.html">Contact</a></li>
+        </ul>
+        <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+      </nav>
+    </div>
+  </header>
+
+  <main class="main">
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section">
+
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6 text-center" data-aos="fade-up" data-aos-delay="100">
+            <h2><span>Travel Itineraries</span></h2>
+            <p>Let us help you plan an epic itinerary in one of the many places around the world that we love!</p>
+            <a href="contact.html" class="btn-get-started">Available for Hire<br></a>
+          </div>
+        </div>
+      </div>
+
+    </section><!-- /Hero Section -->
+
+    <!-- Gallery Section -->
+    <section id="gallery" class="gallery section">
+
+      <div class="container-fluid" data-aos="fade-up" data-aos-delay="100">
+      
+        <div class="row gy-4 justify-content-center"> 
+
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+             
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+
+            <div class="col-xxl-3 col-lg-4 col-md-6 p-5">
+                <div>
+                    <div class="gallery-item">
+                    <img src="assets/img/gallery/gallery-1.jpg" class="img-fluid" alt="">
+                    <div class="gallery-links d-flex align-items-center justify-content-center">
+                        <a href="assets/img/gallery/gallery-1.jpg" title="Gallery 1" class="glightbox preview-link font-body">Find out more!</a>
+                    </div>
+                    </div>
+                </div>
+                <div class="card-body pt-3">
+                    <h4 class="card-title">The Globus British Escape Tour of Great Britain (Full Overview)</h4>
+                    <p class="card-text pt-2">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <p style="font-size: 14px">Posted in <a href="#">England</a>, <a href="#">Europe Travel</a>, <a href="#">Scotland</a></p>
+                    <p style="font-size: 13px"><a href="#">Travel Itineraries</a>, <a href="#">Travel Inspiration</a></p>
+                    <a href="#" class="btn btn-light">Find out more!</a>
+                </div>
+            </div>
+            <!-- End Gallery Item -->
+          
+        </div>
+
+      </div>
+
+    </section><!-- /Gallery Section -->
+
+  </main>
+
+  <footer id="footer" class="footer">
+
+    <div class="container">
+      <div class="copyright text-center ">
+        <p>© <span>Copyright</span> <strong class="px-1 sitename">Teteng Finance.</strong> <span>All Rights Reserved</span></p>
+      </div>
+  </footer>
+
+  <!-- Scroll Top -->
+  <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div class="line"></div>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+
+  <!-- Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
