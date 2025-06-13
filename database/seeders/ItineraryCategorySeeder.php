bf227e586b42ec0d9b500862faf0fd62cfc10f4a
aldrvk <aldriknoelsianipar@gmail.com>
2025-06-13 15:38:11 +0700

    membuat agar list dropdown bagus

---
+++ database/seeders/ItineraryCategorySeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class ItineraryCategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $parentCategories = [
+            101 => 'Africa Travel',
+            102 => 'Asia Travel',
+            103 => 'Europe Travel',
+            104 => 'North America Travel',
+            105 => 'South America Travel',
+            106 => 'Central America Travel',
+            107 => 'Caribbean Travel',
+            108 => 'Middle East Travel',
+            109 => 'Southeast Asia Travel',
+            110 => 'Oceania Travel',
+            111 => 'Antarctica',
+            112 => 'United States',
+        ];
+
+        $insertData = [];
+
+        foreach ($parentCategories as $id => $title) {
+            $slug = $this->generateUniqueSlug($title);
+
+            $insertData[] = [
+                'id' => $id,
+                'title' => $title,
+                'slug' => $slug,
+                'parent_id' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ];
+        }
+
+        DB::table('itinerary_categories')->insert($insertData);
+    }
+
+    private function generateUniqueSlug(string $title): string
+    {
+        $slug = Str::slug($title);
+        $originalSlug = $slug;
+        $i = 1;
+
+        while (DB::table('itinerary_categories')->where('slug', $slug)->exists()) {
+            $slug = $originalSlug . '-' . $i;
+            $i++;
+        }
+
+        return $slug;
+    }
+}
--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -10,41 +10,42 @@
           <li class="px-3"><a href="{{ route('landing') }}" class="active">Travel Itineraries<br></a></li>
           <li class="px-3"><a href="about.html">About</a></li>
           <li class="dropdown px-3"><a href=""><span>Destinations</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-            <ul>
-
-            @if(isset($categories) && $categories->isNotEmpty())
-            @foreach ($categories as $parent)
-                @if ($parent->children->isNotEmpty())
-                    <li class="dropdown"><a href="{{ route('itinerary.category.nested', $parent->slug) }}"><span>{{ $parent->title }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                        <ul>
-                            @foreach ($parent->children as $child)
-                                @if ($child->children->isNotEmpty())
-                                    <li class="dropdown"><a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug]) }}"><span>{{ $child->title }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
-                                        <ul>
-                                            @foreach ($child->children as $grandchild)
-                                                <li class="dropdown"><a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug, $grandchild->slug]) }}"><span>{{ $grandchild->title }}</span></a>
-                                                </li>
-                                            @endforeach
-                                        </ul>
-                                    </li>
-                                @else
-                                    <li class="dropdown">
-                                        <a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug]) }}">{{ $child->title }}</a>
-                                    </li>
-                                @endif
-                            @endforeach
-                        </ul>
-                    </li>
-                @else
-                    <li>
-                        <a class="dropdown-item" href="{{ route('itinerary.category.nested', $parent->slug) }}">
-                            {{ $parent->title }}
-                        </a>
-                    </li>
+            <ul class="dropdown-list multi-column grid">
+                @if(isset($categories) && $categories->isNotEmpty())
+                @php
+                    $chunks = $categories->chunk(ceil($categories->count() / 2));
+                @endphp
+                    @foreach ($categories as $parent)
+                        @if ($parent->children->isNotEmpty())
+                            <li class="dropdown"><a href="{{ route('itinerary.category.nested', $parent->slug) }}"><span>{{ $parent->title }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                <ul>
+                                    @foreach ($parent->children as $child)
+                                        @if ($child->children->isNotEmpty())
+                                            <li class="dropdown"><a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug]) }}"><span>{{ $child->title }}</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                                                <ul class="sub-dropdown">
+                                                    @foreach ($child->children as $grandchild)
+                                                        <li class="dropdown"><a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug, $grandchild->slug]) }}"><span>{{ $grandchild->title }}</span></a>
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            </li>
+                                        @else
+                                            <li class="dropdown">
+                                                <a href="{{ route('itinerary.category.nested', [$parent->slug, $child->slug]) }}">{{ $child->title }}</a>
+                                            </li>
+                                        @endif
+                                    @endforeach
+                                </ul>
+                            </li>
+                        @else
+                            <li>
+                                <a class="dropdown-item" href="{{ route('itinerary.category.nested', $parent->slug) }}">
+                                    {{ $parent->title }}
+                                </a>
+                            </li>
+                        @endif
+                    @endforeach
                 @endif
-            @endforeach
-            @endif
-
             </ul>
           </li>
           <li class="px-3"><a href="#">Services</a></li>
